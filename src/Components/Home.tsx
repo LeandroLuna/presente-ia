278326f5d061ac2b6b73ec6dad948b9ffda52268
Leandro Luna <leandro.j.luna@gmail.com>
2022-12-29 00:14:05 -0300

    add home initial compoennt

---
+++ src/Components/Home.tsx
@@ -0,0 +1,11 @@
+import { Center} from '@chakra-ui/react'
+
+function Home(){
+    return(
+        <Center h='100vh' bg='black' color='white'>
+            <h1>Home component created!</h1>
+        </Center>
+    );
+}
+
+export default Home;
